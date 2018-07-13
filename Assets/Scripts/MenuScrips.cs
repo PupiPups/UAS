53a0ea3138a9305689e443e03d2c9ef9beb9e952
Emanuel <emanuel_318@hotmail.com>
2018-07-13 20:21:19 -0300

    menu editado

---
+++ Assets/Scripts/MenuScrips.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScrips : MonoBehaviour {
+    public RectTransform grupo1;
+    public RectTransform grupo2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void CambiarPosicion()
+    {
+        grupo1.GetComponent<RectTransform>().localPosition = Vector3.zero;
+
+        grupo2.GetComponent<RectTransform>().localPosition = new Vector3(700f, 700f, 700f);
+
+    }
+}
