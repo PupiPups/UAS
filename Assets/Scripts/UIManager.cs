8ad9931fd529b320b9f91e5a343b9f491b776421
Santiago Aguirre <Santiaguirre10@users.noreply.github.com>
2018-07-13 20:13:18 -0300

    UI Gameplay

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+    public Image lifeBar;
+    public PlayerData playerData;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeBar.fillAmount = 0.01f * playerData.life;
+	}
+    public void backMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+}
