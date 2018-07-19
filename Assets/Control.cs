7143adc2e7b96401c8eda6158a762794a3542ed1
Emanuel <emanuel_318@hotmail.com>
2018-07-19 03:51:51 -0300

    Player Select base

---
+++ Assets/Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Control : MonoBehaviour {
+    private Image[] im;
+    public Text titulo;
+    // Use this for initialization
+    void Start () {
+        im = FindObjectsOfType<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SelectClass(int a){
+        
+        switch (a){
+            
+            case 1:  //perro
+                PlayerPrefs.SetInt("PlayerClass", 1);
+                break;
+            case 2: //gato
+                PlayerPrefs.SetInt("PlayerClass", 2);
+                break;
+            case 3: //panda
+                PlayerPrefs.SetInt("PlayerClass", 3);
+                break;
+            case 4:
+                PlayerPrefs.SetInt("PlayerClass", 4);
+                break;
+        }
+        foreach (Image s in im){
+            s.gameObject.SetActive(false);
+        }
+        titulo.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerOptions.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOptions : MonoBehaviour {
+    public int clase;
+	// Use this for initialization
+	void Start () {
+        clase = PlayerPrefs.GetInt("PlayerClass");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void basico()
+    {
+        switch (clase)
+        {
+
+            case 1:  //perro
+                //codigo de ataque basico
+                break;
+            case 2: //gato
+                    //codigo de ataque basico
+                break;
+            case 3: //panda
+                    //codigo de ataque basico
+                break;
+            case 4:
+                //codigo de ataque basico
+                break;
+        }
+
+    }
+
+    public void poder1()
+    {
+        switch (clase)
+        {
+
+            case 1:  //perro
+                //codigo de poder 1
+                break;
+            case 2: //gato
+                    //codigo de poder 1
+                break;
+            case 3: //panda
+                    //codigo de poder 1
+                break;
+            case 4:
+                //codigo de poder 1
+                break;
+        }
+
+    }
+
+    public void poder2()
+    {
+        switch (clase)
+        {
+
+            case 1:  //perro
+                //codigo de poder 2
+                break;
+            case 2: //gato
+                    //codigo de poder 2
+                break;
+            case 3: //panda
+                    //codigo de poder 2
+                break;
+            case 4:
+                //codigo de poder 2
+                break;
+        }
+
+    }
+
+    public void poder3()
+    {
+        switch (clase)
+        {
+
+            case 1:  //perro
+                //codigo de poder 3
+                break;
+            case 2: //gato
+                    //codigo de poder 3
+                break;
+            case 3: //panda
+                    //codigo de poder 3
+                break;
+            case 4:
+                //codigo de poder 3
+                break;
+        }
+
+    }
+
+}
