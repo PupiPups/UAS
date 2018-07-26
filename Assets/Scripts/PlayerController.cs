2689809746c08ba17bcc7e856ed34bf40fde8dd3
Santiago Aguirre <Santiaguirre10@users.noreply.github.com>
2018-07-26 16:05:11 -0300

    Joystick virtuales y correcciones de UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float moveHorizontal;
     public float moveVertical;
     public Rigidbody rb;
-
+    private Vector3 offset;
+    public GameObject camera;
     void Start()
     {
-       
+        offset = camera.transform.position - this.transform.position;
     }
 
     void FixedUpdate()
     {
-        moveHorizontal = Input.GetAxis("Horizontal");
-        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        moveVertical = CrossPlatformInputManager.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //transform.Translate()
         rb.AddForce(movement * speed);
+    }
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        camera.transform.position = this.transform.position + offset;
     }
 }
