aecb146150f840bf7adc0bce3d821706443d71b6
Emanuel <emanuel_318@hotmail.com>
2018-08-06 08:02:16 -0300

    edicion del menu y musica

--- Assets/Scripts/MenuScrips.cs
+++ Assets/Scripts/MenuScrips.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScrips : MonoBehaviour {
     public RectTransform grupo1;
     public RectTransform grupo2;
+    public AudioSource sonido;
 	// Use this for initialization
 	void Start () {
 		
@@ -21,5 +23,18 @@ public class MenuScrips : MonoBehaviour {
 
         grupo2.GetComponent<RectTransform>().localPosition = new Vector3(700f, 700f, 700f);
 
+    }
+    public void salir()
+    {
+        Application.Quit();
+    }
+
+    public void efecto()
+    {
+        sonido.Play();
+    }
+
+    public void singleplayer() {
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/control_sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control_sound : MonoBehaviour {
+    public AudioSource[] musica;
+	// Use this for initialization
+	void Start () {
+        musica = FindObjectsOfType<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	public void silenciar () {
+		    foreach(AudioSource s in musica)
+        {
+            s.Stop();
+
+        }
+	}
+}
